e4ec63b175ae2743a2fd30525086e4cb33a43667
SeleDreams <seledreams.contact@gmail.com>
2024-09-26 14:32:34 +0200

    fixed issue with integer deltatime with flecs

--- Engine/core/scene/scene.c
+++ Engine/core/scene/scene.c
@@ -7,6 +7,7 @@
 
 void csSceneInit(csScene *p_scene) {
     p_scene->world = ecs_init();
+    ecs_measure_frame_time(p_scene->world,false);
     ECS_IMPORT(p_scene->world,csCamera);
     csCameraCreate(p_scene->world);
 }
@@ -17,7 +18,7 @@ void csSceneDestroy(csScene *p_scene) {
 }
 
 void csSceneUpdate(csScene *p_scene,csFixed delta) {
-    ecs_progress(p_scene->world, csFixedToFloat(delta));
+    ecs_progress(p_scene->world, delta);
 }
 
 void csSceneStart(csScene *p_scene) {
